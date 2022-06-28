51b904dea9bdddffa49760672c4451e90134455a
AdrieliKet <adrielikethin.dossantos@gmail.com>
2022-06-27 22:37:54 -0300

    Adicionando firebase

--- lib/app/domain/service/controlarCodigoInicial.dart
+++ lib/app/domain/service/controlarCodigoInicial.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 //gabi
 //Essa é a versão inicial do código, já visulizada pelo prof Helio
 void main(List<String> args) {
@@ -11,7 +12,7 @@ void main(List<String> args) {
         2- Registro Pressão ''');
 
   //estou solicitando a opção do menu que o usuário escolheu
-  String respostaOpMenu;
+  String? respostaOpMenu;
   respostaOpMenu = null;
   do {
     print('Entrada não pode ser nula');
@@ -23,7 +24,7 @@ void main(List<String> args) {
   print('''Você está em jejum? 
               1- Sim 
               2- Não''');
-  String respostajejum;
+  String? respostajejum;
   respostajejum = null;
   do {
     print('Entrada não pode ser nula');
@@ -33,7 +34,7 @@ void main(List<String> args) {
 
 //estou perguntando o valor da glicemia do paciente
   print('Informe o valor de sua Glicemia: ');
-  String respostaValorGlicemia;
+  String? respostaValorGlicemia;
   respostaValorGlicemia = null;
   do {
     print('Entrada não pode ser nula');
@@ -44,7 +45,7 @@ void main(List<String> args) {
 //estou perguntando o valor da pressão arterial do paciente
   print(
       'Informe o valor de sua pressão arterial, ao invês de saparar por / insira uma virgula no local: ');
-  String respostaValorPressao;
+  String? respostaValorPressao;
   respostaValorPressao = null;
   do {
     print('Entrada não pode ser nula');
@@ -53,38 +54,57 @@ void main(List<String> args) {
   valorPressao = double.parse(respostaValorPressao);
 
   //implementação dos if do que o sistema fará
-  if (opcaomenu == 1) verificarGlicemia(estaEmJejum, valorGlicemia);
-  if (opcaomenu == 2) verificarPressao(valorPressao);
-} 
+  /*if (opcaomenu == 1) verificarGlicemia(estaEmJejum, valorGlicemia);
+  if (opcaomenu == 2) verificarPressao(valorPressao); */
+}
 //gabi ate aqui
 
 //Adrieli
 // estou verificando a glicemia se a pessoa não esta em jejum
 String verificarGlicemiaNaoJejum(int valorGlicemia) {
-  if (valorGlicemia < 70) return ('Hipoglicemia');
-  if (valorGlicemia >= 70 && valorGlicemia <= 140) return ('Normal');
-  if (valorGlicemia > 140 && valorGlicemia <= 200) return ('Pré-Diabetes');
-  if (valorGlicemia > 200) return ('Diabetes');
+  if (valorGlicemia < 70) {
+    return 'Hipoglicemia';
+  } else if (valorGlicemia >= 70 && valorGlicemia <= 140) {
+    return 'Normal';
+  } else if (valorGlicemia > 140 && valorGlicemia <= 200) {
+    return 'Pré-Diabetes';
+  } else if (valorGlicemia > 200) {
+    return 'Diabetes';
+  } else {
+    return 'Valor incorreto!';
+  }
 }
 
 // estou verificando a glicemia se a pessoa esta em jejum
 String verificarGlicemiaJejum(int valorGlicemia) {
-  if (valorGlicemia < 70) return ('Hipoglicemia');
-  if (valorGlicemia >= 70 && valorGlicemia <= 100) return ('Normal');
-  if (valorGlicemia > 100 && valorGlicemia <= 126) return ('Pré-Diabetes');
-  if (valorGlicemia > 126) throw Exception('Diabetes');
+  if (valorGlicemia < 70) {
+    return ('Hipoglicemia');
+  } else if (valorGlicemia >= 70 && valorGlicemia <= 100) {
+    return ('Normal');
+  } else if (valorGlicemia > 100 && valorGlicemia <= 126) {
+    return ('Pré-Diabetes');
+  } else if (valorGlicemia > 126) {
+    return ('Diabetes');
+  } else {
+    return 'Valor incorreto!';
+  }
 }
 
-
-//rafaela 
+//rafaela
 String verificarPressao(double valorPressao) {
-  if (valorPressao == 120.60) return 'Valor ideal';
-  if (valorPressao < 120.60) return ('Abaixo da média');
-  if (valorPressao > 130.85 && valorPressao < 139.89)
+  if (valorPressao == 120.60) {
+    return 'Valor ideal';
+  } else if (valorPressao < 120.60) {
+    return ('Abaixo da média');
+  } else if (valorPressao > 130.85 && valorPressao < 139.89) {
     return ('Normal Limítrofe');
-  if (valorPressao >= 140.90 && valorPressao <= 159.99)
+  } else if (valorPressao >= 140.90 && valorPressao <= 159.99) {
     return ('Hipertensão leve (estágio 1) ');
-  if (valorPressao >= 160.100 && valorPressao < 180.110)
+  } else if (valorPressao >= 160.100 && valorPressao < 180.110) {
     return ('Hipertensão moderada (estágio 2)');
-  if (valorPressao >= 180.110) return ('Hipertensão grave (estágio 3)');
+  } else if (valorPressao >= 180.110) {
+    return ('Hipertensão grave (estágio 3)');
+  } else {
+    return 'Valor incorreto!';
+  }
 }
